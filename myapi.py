57b500bf5a007e6189e23271ebb01cd0414cdb7f
Emanuel Maia <tavanomanu@gmail.com>
2024-03-01 16:52:51 +0000

    add fast api tutorial

---
+++ myapi.py
@@ -0,0 +1,76 @@
+from fastapi import FastAPI, Path
+from typing import Optional
+from pydantic import BaseModel
+
+
+app = FastAPI()
+
+students = {
+    1: {
+        "name": "John",
+        "age": 17,
+        "year": "year 12"
+    }
+}
+
+class Student(BaseModel):
+    name: str
+    age: int
+    year: str
+
+class UpdateStudent(BaseModel):
+    name: Optional[str] = None
+    age: Optional[int] = None
+    year: Optional[str] = None
+
+@app.get("/")
+def index():
+    return {"name": "First Data"}
+
+@app.get("/get-student/{student_id}")
+def get_student(student_id: int = Path(..., description="The ID of the student you want to view", gt=0, lt=3)):
+    if student_id not in students:
+        return {"Error": "Student does not exist"}
+
+    return students[student_id]
+
+@app.get("/get-by-name")
+def get_student_by_name(*, student_id:int, name: Optional[str] = None, test: int):
+                        # use this asterisk when mixing optional
+                        # and required query parameters
+    for student_id in students:
+        if students[student_id]["name"] == name:
+            return students[student_id]
+    return {"Data": "Not found"}
+
+@app.post("/create-student/{student_id}")
+def create_student(student_id: int, student: Student):
+    if student_id in students:
+        return {"Error": "Student exists"}
+
+    students[student_id] = student.dict() # No need for model_dump, directly convert to dictionary
+    return students[student_id]
+
+@app.put("/update-student/{student_id}")
+def update_student(student_id: int, student: UpdateStudent):
+    # IMPORTANT:
+    # can't leave trailing comma in the update on FastAPI for some reason
+
+    if student_id not in students:
+        return {"Error": "Student doesn't exist"}
+
+    # Update only the fields that are provided
+    for field, value in student.dict().items():
+        if value is not None:
+            students[student_id][field] = value
+
+    return students[student_id]
+
+@app.delete("/delete-student/{student_id}")
+def delete_student(student_id: int):
+    if student_id not in students:
+        return {"Error": "Student does not exist"}
+
+    del students[student_id]
+    return {"Message": "Student deleted successfully"}
+
